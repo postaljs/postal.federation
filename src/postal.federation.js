efb63bc9ca784842476555898c5ddbcebfe995ac
Jim Cowart <ashbylane@gmail.com>
2013-01-07 21:41:00 -0500

    Updated to work with new structure

--- src/postal.federation.js
+++ src/postal.federation.js
@@ -1,17 +1,19 @@
 (function ( root, factory ) {
   if ( typeof module === "object" && module.exports ) {
     // Node, or CommonJS-Like environments
-    module.exports = factory( require( "underscore" ), require( "postal" ) );
+    module.exports = function(_, postal, riveter) {
+      return factory( _, postal, riveter );
+    };
   } else if ( typeof define === "function" && define.amd ) {
     // AMD. Register as an anonymous module.
-    define( ["underscore", "postal"], function ( _, postal ) {
-      return factory( _, postal, root );
+    define( ["underscore", "postal", "riveter"], function ( _, postal, riveter ) {
+      return factory( _, postal, riveter, root );
     } );
   } else {
     // Browser globals
-    root.postal = factory( root._, root.postal, root );
+    root.postal = factory( root._, root.postal, root.riveter, root );
   }
-}( this, function ( _, postal, global, undefined ) {
+}( this, function ( _, postal, riveter, global, undefined ) {
 
   //import("federation.js");
 
