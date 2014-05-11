467cc6584d3cf290223a5e007f5cc9df95ff4d7a
ifandelse <jim@ifandelse.com>
2014-05-11 00:34:45 -0400

    Updated build to gulp, minor refactoring to adpat to linting

--- src/postal.federation.js
+++ src/postal.federation.js
@@ -1,22 +1,22 @@
-(function ( root, factory ) {
-  if ( typeof module === "object" && module.exports ) {
-    // Node, or CommonJS-Like environments
-    module.exports = function(_, postal, riveter) {
-      return factory( _, postal, riveter );
-    };
-  } else if ( typeof define === "function" && define.amd ) {
-    // AMD. Register as an anonymous module.
-    define( ["underscore", "postal", "riveter"], function ( _, postal, riveter ) {
-      return factory( _, postal, riveter, root );
-    } );
-  } else {
-    // Browser globals
-    root.postal = factory( root._, root.postal, root.riveter, root );
-  }
-}( this, function ( _, postal, riveter, global, undefined ) {
+(function(root, factory) {
+    if (typeof module === "object" && module.exports) {
+        // Node, or CommonJS-Like environments
+        module.exports = function(_, postal, riveter) {
+            return factory(_, postal, riveter);
+        };
+    } else if (typeof define === "function" && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(["lodash", "postal", "riveter"], function(_, postal, riveter) {
+            return factory(_, postal, riveter, root);
+        });
+    } else {
+        // Browser globals
+        root.postal = factory(root._, root.postal, root.riveter, root);
+    }
+}(this, function(_, postal, riveter, global, undefined) {
 
-  //import("federation.js");
+    //import("federation.js");
 
-  return postal;
+    return postal;
 
-} ));
+}));
